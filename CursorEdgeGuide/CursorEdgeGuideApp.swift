a684eeebdc9aab75cbe1e8277bd439e062d9f44e
Yusuke Majima <yusuke.majima@gmail.com>
2026-02-26 22:32:16 +0900

    Initial Commit

---
+++ CursorEdgeGuide/CursorEdgeGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorEdgeGuideApp.swift
+//  CursorEdgeGuide
+//
+//  Created by Yusuke Majima on 2026/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CursorEdgeGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
